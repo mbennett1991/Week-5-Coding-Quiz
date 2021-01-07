ea8ded4e4fbaf2328cfee9c82571581039b4ad49
Elle <bound_by_harmony@yahoo.com>
2021-01-07 21:29:22 +0000

    Global Variable Defined

---
+++ script.js
@@ -0,0 +1,13 @@
+var quiz;
+var timer;
+var startBtn;
+var quizStatus;
+var choices;
+var choiceA;
+var choiceB;
+var choiceC;
+var userScore = 0;
+var totalPossibleScore = 100;
+var correctAnswer = 20;
+var userInitials; 
+
