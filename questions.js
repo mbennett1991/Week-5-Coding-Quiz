9f651daecd86a898b38e4f27b8e3c6b15f4f769b
Elle <bound_by_harmony@yahoo.com>
2021-01-17 16:13:33 +0000

    Questions array

---
+++ questions.js
@@ -0,0 +1,27 @@
+var quizQuestions [
+    {
+        question: "What does HTML stand for?",
+        choices: ["HyperText Markup Link", "HyperText Markup Language", "HyperText Markup Lingo", "HyperText Master Language"],
+        correctAnswer: 1
+    },
+    {
+        question: "What does CSS stand for?",
+        choices: ["Cascading Style Sheet", "Cool Style Sheet", "Cascading Style Syntax", "Class Style Sheet"],
+        correctAnswer: 0
+    },
+    {
+        question: "What is JavaScript and what does it do?",
+        choices: ["A Programming Language", "Change and Update both HTML and CSS", "Calculates, Manipulates and Validates data", "All of the above"],
+        correctAnswer: 3
+    },
+    {
+        question: "What is Bootstrap?",
+        choices: ["A design program language", "A part of a shoe", "A popular CSS framework", "A phone app"],
+        correctAnswer: 2
+    },
+    {
+        question: "What is the data-type for the following '1'?",
+        choices: ["Array", "Boolean", "Integer", "String"],
+        correctAnswer: 3
+    }
+];
