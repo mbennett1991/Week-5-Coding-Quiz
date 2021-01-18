4e527c360ad25c1e09a1e7207a11dc4dd4f81604
Elle <bound_by_harmony@yahoo.com>
2021-01-18 01:15:12 +0000

    Display Questions Function

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ var startBox = document.getElementById("startScreen")
 var startBtn = document.querySelector(".startBtn button");
 var quizBox = document.getElementById("quizBox");
 var timer = document.querySelector(".timeLeft");
+var resultsPage = document.getElementById("resultsPage");
 
 var quiz;
 var timer;
 var quizStatus;
-var choices;
-var choiceA;
-var choiceB;
-var choiceC;
 var userScore = 0;
 var totalPossibleScore = 100;
-var correctAnswer = 20;
+var correctAnswerPoints = 20;
 var userInitials; 
 var currentIndex = 0; 
 
@@ -33,17 +30,41 @@ function startQuiz(){
 }
 
 function displayQuestions(){
+  var questionHeader = document.createElement("h3");
+  var choiceA = document.createElement("button");
+  var choiceB = document.createElement("button");
+  var choiceC = document.createElement("button");
+  var choiceD = document.createElement("button"); 
   //populate the first question into the questions and answers sections
-  document........
-  .setValue = questions[currentindex].questionTitle
+  
+  questionHeader.setValue = questions
+  choiceA.setValue = choices[0];
+  choiceB.setValue = choices[1];
+  choiceC.setValue = choices[2];
+  choiceD.setValue = choices[3];
+
+  //for (var i = 0; i=quizQuestions.length; i++);
+  for (var i = 0; i = choices.length; i++);
+  choiceA.addEventListener("click", quizQuestions++);
+  choiceB.addEventListener("click", quizQuesions++);
+  choiceC.addEventListener("click", quizQuestions++);
+  choiceD.addEventListener("click", quizQuestions++)
+
   //worth noting that you will need to use a forEach on the choices array in the questions object your are displaying
   //you will also need to create an onclick on each of these buttons which wll call another function
 
-}
+};
+displayQuestions();
+
 
 function checkAnswer(){
 // check if the answer is correct or not...
-
+if (choiceA || choiceB || choiceC || choiceD === correctAnswer){
+  userScore ++
+  correctAnswerPoints ++
+}else {
+  return;
+}
 // increment the current index 
 //check if we are out of questions and if so.... call the end of quiz function... if notcall displatQuestions 
 
